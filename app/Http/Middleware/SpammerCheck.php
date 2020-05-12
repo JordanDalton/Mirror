506144d32aa3390c33df899cbc40bb1d8d0b1db7
Jordan Dalton <jordan.dalton@ymail.com>
2020-05-11 19:25:08 -0500

    Ready To Test

---
+++ app/Http/Middleware/SpammerCheck.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SpammerCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $referer = request()->headers->get('referer');
+
+        if($referer)
+        {
+            if($referer === 'https://rirofiwal.cf')
+            {
+                return response()->json(['DENIED'],403);
+            }
+        }
+
+        return $next($request);
+    }
+}
